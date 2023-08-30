c5aafcd20c1a472b665d9d581746c27d4c7343fa
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 22:30:29 +1000

    Adds example data

--- build/db-handler/app/routers/figures_router.py
+++ build/db-handler/app/routers/figures_router.py
@@ -1,19 +1,25 @@
-from fastapi import APIRouter
+from fastapi import Body, APIRouter
 from app.dependencies.database import cal_col, user_col
-from datetime import datetime
 import time as time_lib
-import app.constants as constants
 import app.utils as utils
+from typing import Annotated
 
 router = APIRouter(
     prefix="/figurines",
-    tags=["figurines"]
+    tags=["Figurines"]
 )
 
 
-@router.get("")
-async def get_available():
-    print(utils.current_to_timeslot_num())
+@router.get("", summary="Gets map of all users and their availability for that timeslot")
+async def get_available() -> Annotated[dict, Body(
+    examples=[
+        {
+            "user_1": 1,
+            "user_2": 0,
+            "user_3": 1
+        }
+    ]
+)]:
     users: dict = {}
     timeslot: str = utils.current_to_timeslot()
     booked_users = cal_col.find_one({"day": time_lib.strftime("%A").lower(), "time": timeslot}).get("booked_users")
