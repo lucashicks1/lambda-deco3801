ec82a133fad6a5065cd7dde33f310554291c9450
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 22:29:08 +1000

    Makes code work

--- build/figurines/figurine_poller.py
+++ build/figurines/figurine_poller.py
@@ -6,19 +6,23 @@ figurine_status = {}
 response = {}
 POLL_DELAY = 15
 
-while True:
-    print("Polling now")
-    response = requests.get("http://127.0.0.1:8000/figurines")
-    if response.text == figurine_status:
-        print("NO CHANGE")
-    else:
-        print("IT CHANGED!!!!")
-        figurine_status = response.text
-        print(figurine_status)
-        send_to_display(figurine_status)
-        # Do pyserial stuff
-    # Sleep for 15 seconds
-    sleep(POLL_DELAY)
-
 def send_to_display(figurine_status: dict):
     print(figurine_status)
+
+def main():
+    while True:
+        print("Polling now")
+        response = requests.get("http://127.0.0.1:8000/figurines")
+        if response.text == figurine_status:
+            print("NO CHANGE")
+        else:
+            print("IT CHANGED!!!!")
+            figurine_status = response.text
+            print(figurine_status)
+            send_to_display(figurine_status)
+            # Do pyserial stuff
+        # Sleep for 15 seconds
+        sleep(POLL_DELAY)
+
+if __name__ == "__main__":
+    main()
