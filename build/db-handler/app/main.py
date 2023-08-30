35a827077874fae52a5b46cb5f2e70726b895a34
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 16:39:05 +1000

    minor fixes

--- build/db-handler/app/main.py
+++ build/db-handler/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.dependencies.database import cal_col
+from uuid import uuid4
 
 app = FastAPI()
 
+test_dict = {"name": str(uuid4())}
 
 @app.get("/")
 async def test():
+    cal_col.insert_one(test_dict)
     return {"message": "Dylan loves deco!!!"}
