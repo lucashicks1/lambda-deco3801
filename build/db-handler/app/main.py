d6bd6ce4fdf1c6e7666ec72cdad321b3b4c69fee
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 19:14:32 +1000

    Adds router for figurines

--- build/db-handler/app/main.py
+++ build/db-handler/app/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
-from app.dependencies.database import cal_col
 from uuid import uuid4
 
-app = FastAPI()
+from fastapi import FastAPI
+from app.routers import figures_router
+from app.dependencies.database import cal_col
 
-test_dict = {"name": str(uuid4())}
+tags_metadata = [
+    {
+        "name": "Figure Display Endpoints",
+        "description": "All endpoints will be used by the figurine display"
+    }
+]
+
+app = FastAPI(
+    title="Lambda DB Handler",
+    openapi_tags=tags_metadata
+)
+
+app.include_router(figures_router.router)
 
 @app.get("/")
 async def test():
+    test_dict = {"name": str(uuid4())}
     cal_col.insert_one(test_dict)
     return {"message": "Dylan loves deco!!!"}
