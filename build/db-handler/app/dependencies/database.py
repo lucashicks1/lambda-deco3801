c12e48500d77c73c4dcfe44bf59c9aa137e00899
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 17:28:05 +1000

    Adds collection to database

--- build/db-handler/app/dependencies/database.py
+++ build/db-handler/app/dependencies/database.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient, collection
 DB_HOST = "localhost"
 DB_PORT = 27017
 DB_NAME = "mydb"
-COL_NAME = "calendar"
 
 client = MongoClient(DB_HOST, DB_PORT)
 
-cal_col: collection = client[DB_NAME][COL_NAME]
+cal_col: collection = client[DB_NAME]["calendar"]
+user_col: collection = client[DB_NAME]["users"]
