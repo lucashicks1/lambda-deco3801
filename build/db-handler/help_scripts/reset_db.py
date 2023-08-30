47f1eb482f47799d263ab29f9753c2a9aee99d4a
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-30 19:13:57 +1000

    Modifies script that resets db

--- build/db-handler/help_scripts/reset_db.py
+++ build/db-handler/help_scripts/reset_db.py
@@ -9,16 +9,20 @@ user_col.delete_many({})
 for day in DAYS:
     minute: int = 0
     hour: int = 0
+    timeslot_num: int = 0
     while hour < 24:
-        cal_col.insert_one({
+        document = {
             "day": day,
             "time": f"{hour:02}:{minute:02}",
+            "slot_num": timeslot_num,
             "booked_users": []
-        })
+        }
+        cal_col.insert_one(document)
         minute += constants.TIMESLOT_LEN
         if minute == 60:
             minute = 0
             hour += 1
+        timeslot_num += 1
 
 cal_col.update_many(filter={}, update={"$set": {"booked_users": [constants.USERS[0]]}})
 
