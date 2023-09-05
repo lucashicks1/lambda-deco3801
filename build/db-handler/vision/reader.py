db85fb838e327246bfd669debf7d99962ef2854d
alexvillr <73649305+alexvillr@users.noreply.github.com>
2023-09-05 23:08:18 +1000

    Fixed up python script. Added image I used for testing as well as output json. Also added environment for my conda env

--- build/db-handler/vision/reader.py
+++ build/db-handler/vision/reader.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import json
+import matplotlib.pyplot as plt
 import numpy as np
+import pytesseract
 
 days_of_the_week = {
     0: "Monday",
@@ -13,51 +15,99 @@ days_of_the_week = {
 }
 
 
-def is_coloured(pixel: (float,), threshold: float) -> bool:
-    """
-    is_coloured()
-    -------------
-    A function to determine if our time block has been coloured in to be
-    marked as busy or not.
-    """
-    return sum(pixel) > threshold
-
-
-def main():
+def main(image: Image):
     threshold = 100  # TODO This is summed RGB values. Think of intensity
-    image_path = "./current/current.png"  # TODO set an actual file name
-    image = Image.open(image_path)
     rgb_image = image.convert("RGB")
     # TODO Once opened, or maybe after closed. We should move image out of this
     # folder
 
+    """
+    This will contain real tuples of start x, y and end x, y defining top
+    left and bottom right corners of pixel locations for each time slot.
+    TODO: Consider a way to automate this? Maybe using a class approach?
+    """
     day_time_slots = [
-        """
-        This will contain real tuples of start x, y and end x, y defining top
-        left and bottom right corners of pixel locations for each time slot.
-        TODO: Consider a way to automate this? Maybe using a class approach?
-        """(
-            (0, 0), (140, 30)
-        ),
+        ((420 * 0, 0), (420 * 1, 480)),
+        ((420 * 1, 0), (420 * 2, 480)),
+        ((420 * 2, 0), (420 * 3, 480)),
+        ((420 * 3, 0), (420 * 4, 480)),
+        ((420 * 4, 0), (420 * 5, 480)),
+        ((420 * 5, 0), (420 * 6, 480)),
+        ((420 * 6, 0), (420 * 7, 480)),
     ]
+    day_time_crops = []
     coloured_time_slots = []
-    time_slot_width = 140  # TODO Can also maybe be automated?
+    time_slot_height = 480
 
     for day, (top_left, bottom_right) in enumerate(day_time_slots):
-        for time_slot in range(0, 23):  # 24 hour times
-            x = top_left[0] + time_slot * time_slot_width
-            y = top_left[1]
-
-            pixel_colour = rgb_image.getpixel(x, y)
-            if is_coloured(pixel_colour, threshold):
-                coloured_time_slots.append({
-                    'day': days_of_the_week.get(day),
-                    'time_slot': time_slot,
-                    'colour': pixel_colour
-                    })
-    with open('coloured_time_slots.json', 'w') as json_file:
+        time_crops = []
+        for time_slot in range(0, 5):
+            time_slot_crop = image.crop(
+                (
+                    top_left[0],
+                    top_left[1] + time_slot * time_slot_height,
+                    bottom_right[0],
+                    bottom_right[1] + time_slot * time_slot_height,
+                )
+            )
+            time_slot_array = np.array(time_slot_crop)
+            time_slot_size = time_slot_array.shape[0] * time_slot_array.shape[1]
+            threshold_mask = (time_slot_array <= threshold).all(axis=-1)
+            num_coloured = np.sum(threshold_mask)
+            is_coloured = (num_coloured / time_slot_size) * 100 >= 3
+            time_crops.append((time_slot_crop, is_coloured))
+            if is_coloured:
+                ocr_result = pytesseract.image_to_string(time_slot_crop)
+                coloured_time_slots.append(
+                    {
+                        "day": days_of_the_week.get(day),
+                        "time_slot": time_slot,
+                        "data": ocr_result,
+                    }
+                )
+        day_time_crops.append(time_crops)
+    fig, axes = plt.subplots(5, 7, figsize=(8, 12))
+    axis_index = 0
+    print("Mon\tTue\tWed\tThu\tFri\tSat\tSun")
+    for time in range(5):
+        print(
+            f"{day_time_crops[0][time][1]}\t{day_time_crops[1][time][1]}\t{day_time_crops[2][time][1]}\t{day_time_crops[3][time][1]}\t{day_time_crops[4][time][1]}\t{day_time_crops[5][time][1]}\t{day_time_crops[6][time][1]}"
+        )
+    for i in range(5):
+        for j in range(7):
+            axes.flat[axis_index].imshow(day_time_crops[j][i][0])
+            axes.flat[axis_index].axis("off")
+            axis_index += 1
+    plt.tight_layout()
+    plt.show()
+    with open("coloured_time_slots.json", "w") as json_file:
         json.dump(coloured_time_slots, json_file, indent=4)
 
 
+def normalise_image() -> Image:
+    img_path = "./PXL_20230905_085907760.MP.jpg"
+    img = Image.open(img_path)
+
+    left = 73
+    top = 809
+    bottom = 3258
+    right = 3010
+
+    img = img.crop((left, top, right, bottom))
+    img_array = np.array(img)
+    threshold = 150
+
+    white_mask = (img_array >= threshold).all(axis=-1)
+    img_array[white_mask] = [255, 255, 255]
+    img_array[~white_mask] = [0, 0, 0]
+    new_img = Image.fromarray(img_array)
+
+    return new_img
+
+
 if __name__ == "__main__":
-    main()
+    image = normalise_image()
+    # plt.imshow(image)
+    # plt.grid("on")
+    # plt.show()
+    main(image)
