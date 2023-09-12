aa6365dc14e9a3a72110d965613c7a5842600d93
Dylan Fleming <s4531334@student.uq.edu.au>
2023-09-12 12:23:02 +1000

    Updated microcontroller code to handle multiple cycles of serial communication with piezo buzzer

--- build/microcontroller/atmega328p_lambda_mc/atmega328p_lambda_mc.ino
+++ build/microcontroller/atmega328p_lambda_mc/atmega328p_lambda_mc.ino
@@ -1,65 +1,135 @@
 #include <Servo.h>
+#include "pitches.h"
 
-// Define the digital pins for microcontroller use
+#define MESSAGE_OFFSET 3
+#define SERVO_OFFSET 4
+#define NUM_OF_SERVOS 4
+
+// Define the digital pins for controlling
 const int enableServo1 = 2;
 const int enableServo2 = 3;
 const int enableServo3 = 4;
-const int enableServo4 = 6;
-const int pwmPin = 9; // PWM control pin
+const int enableServo4 = 5;
+const int pwmBuzzerPin = 8; // PWM buzzer control pin
+const int pwmServoPin = 9; // PWM servo control pin
 
-char previousMessage[9] = "";
+char previousMessage[9] = ""; //Previous message
 
-//Use servo library
-Servo servoSignal1;
-Servo servoSignal2;
-Servo servoSignal3;
-Servo servoSignal4;
+Servo servoSignal[NUM_OF_SERVOS]; //Array of servo controllers
+
+/**
+ * family_buzzer_writing(void) 
+**/
+void family_buzzer_writing(void) { //
+  for (int i = 0; i < familyNotes * 2; i = i + 2) {
+
+    // calculates the duration of each note
+    divider = familyMelody[i + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      familyNoteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      familyNoteDuration = (wholenote) / abs(divider);
+      familyNoteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(pwmBuzzerPin, familyMelody[i], familyNoteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(familyNoteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(pwmBuzzerPin);
+  }
+}
+
+void buzzer_writing(int number) {
+  for (int i = 0; i < numOfTones; i++) {
+    int noteDuration = 1000 / userNoteDurations[number][i];
+    int noteStartTime = millis();
+
+    tone(pwmBuzzerPin, userMelody[number][i], noteDuration);
+
+    // Wait for the note to finish playing
+    delay(noteDuration);
+
+    // Stop the tone playing
+    noTone(pwmBuzzerPin);
+
+    // Add a short pause between notes
+    delay(50); // Adjust this value as needed
+  }
+  delay(1000);
+}
+
+
+void servo_write_cycle(char* message) {
+  for (int i = SERVO_OFFSET; i < 8; i++) {
+    int newPosition = (message[i] - '0') * 180; //Set angle for servo motor
+    if (newPosition != servoSignal[i - SERVO_OFFSET].read()) { //If motor angle does not equal new angle
+      servoSignal[i - SERVO_OFFSET].write(newPosition); //Write angle to servo
+      if (newPosition == 0) {
+        buzzer_writing(i - SERVO_OFFSET); //Write to buzzer
+      }
+      delay(500);
+    } else {
+      if (previousMessage[MESSAGE_OFFSET] == '1') { //Check if previous message was a family event and now not
+        if (newPosition == 0) {
+            buzzer_writing(i - SERVO_OFFSET);
+        }
+        delay(500);
+      }
+    }
+  }
+}
+
+void servo_write_reset() {
+  for (int i = MESSAGE_OFFSET; i < 8; i++) {
+    int newPosition = 0;
+    if (newPosition != servoSignal[i - SERVO_OFFSET].read()) {
+      servoSignal[i - SERVO_OFFSET].write(newPosition);
+    }
+  }
+}
 
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
 
-  //Attach servo enable pins
-  servoSignal1.attach(enableServo1);
-  servoSignal2.attach(enableServo2);
-  servoSignal3.attach(enableServo3);
-  servoSignal4.attach(enableServo4);
-  //Write angle of servos to 0
-  servoSignal1.write(0);
-  servoSignal2.write(0);
-  servoSignal3.write(0);
-  servoSignal4.write(0);
+  servoSignal[0].attach(enableServo1);
+  servoSignal[1].attach(enableServo2);
+  servoSignal[2].attach(enableServo3);
+  servoSignal[3].attach(enableServo4);
 
   // Set PWM pin as an output
-  pinMode(pwmPin, OUTPUT);
-  digitalWrite(pwmPin, LOW);
+  pinMode(pwmServoPin, OUTPUT);
+  pinMode(pwmBuzzerPin, OUTPUT);
+  digitalWrite(pwmServoPin, LOW);
 }
 
 void loop() {
-  if (Serial.available() >= 8) { // Check if there are at least 8 characters available in the serial buffer
+  if (Serial.available() >= 😎 { // Check if there are at least 8 characters available in the serial buffer
     char currentMessage[9]; // Buffer to store the received message
     Serial.readBytes(currentMessage, 8); // Read the message into the buffer
     currentMessage[8] = '\0'; // Null-terminate the string
+    analogWrite(pwmServoPin, 255); // Set PWM duty cycle to maximum (255) on pin 9
 
-    if (strncmp(currentMessage, previousMessage, 8) != 0) {
-      if (currentMessage[3] == '1') {
-        analogWrite(pwmPin, 255); // Set PWM duty cycle to maximum (255) on pin 9
-        servoSignal1.write(0);
-        servoSignal2.write(0);
-        servoSignal3.write(0);
-        servoSignal4.write(0);
+    if (strncmp(currentMessage, previousMessage, 😎 != 0) {
+      if ((currentMessage[MESSAGE_OFFSET] == '1')) {
+        if((currentMessage[MESSAGE_OFFSET] != previousMessage[MESSAGE_OFFSET])) {
+          servo_write_reset();
+          family_buzzer_writing();
+        }
         delay(1000);
-        analogWrite(pwmPin, 0); // Turn off PWM on pin 9
       } else {
-        analogWrite(pwmPin, 255); // Set PWM duty cycle to maximum (255) on pin 9
-        servoSignal1.write((currentMessage[4] - '0') * 180);
-        servoSignal2.write((currentMessage[5] - '0') * 180);
-        servoSignal3.write((currentMessage[6] - '0') * 180);
-        servoSignal4.write((currentMessage[7] - '0') * 180);
+        servo_write_cycle(currentMessage);
         delay(1000);
-        analogWrite(pwmPin, 0); // Set PWM duty cycle to maximum (255) on pin 9
       }
       strncpy(previousMessage, currentMessage, 8);
     }
+
+    analogWrite(pwmServoPin, 0); // Turn off PWM on pin 9
   }
 }
