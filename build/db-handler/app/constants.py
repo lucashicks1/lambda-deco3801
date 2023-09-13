aeb79a7ef238db0d9947c22cf6d0f8b5f5ebb74f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-13 17:25:23 +1000

    Adds family user.

--- build/db-handler/app/constants.py
+++ build/db-handler/app/constants.py
@@ -1,3 +1,3 @@
 TIMESLOT_LEN = 15
-USERS = ["user_1", "user_2", "user_3", "user_4"]
+USERS = ["user_1", "user_2", "user_3", "user_4", "family"]
 DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
