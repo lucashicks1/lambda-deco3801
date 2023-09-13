d6eb47d7b10554955c426b4b69c550e3f349824e
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-13 17:29:29 +1000

    Changes.

--- build/db-handler/help_scripts/reset_db.py
+++ build/db-handler/help_scripts/reset_db.py
@@ -1,39 +1,39 @@
-from app.dependencies.database import cal_col, user_col
+from dependencies.database import cal_col, user_col
 import app.constants as constants
 
+def reset_db():
+    cal_col.delete_many({})
+    user_col.delete_many({})
 
-cal_col.delete_many({})
-user_col.delete_many({})
+    for day in constants.DAYS:
+        minute: int = 0
+        hour: int = 0
+        timeslot_num: int = 0
+        while hour < 24:
+            document = {
+                "day": day,
+                "time": f"{hour:02}:{minute:02}",
+                "slot_num": timeslot_num,
+                "booked_users": []
+            }
+            cal_col.insert_one(document)
+            minute += constants.TIMESLOT_LEN
+            if minute == 60:
+                minute = 0
+                hour += 1
+            timeslot_num += 1
 
-for day in constants.DAYS:
-    minute: int = 0
-    hour: int = 0
-    timeslot_num: int = 0
-    while hour < 24:
-        document = {
-            "day": day,
-            "time": f"{hour:02}:{minute:02}",
-            "slot_num": timeslot_num,
-            "booked_users": []
-        }
-        cal_col.insert_one(document)
-        minute += constants.TIMESLOT_LEN
-        if minute == 60:
-            minute = 0
-            hour += 1
-        timeslot_num += 1
+    cal_col.update_many(filter={}, update={"$set": {"booked_users": [constants.USERS[0]]}})
 
-cal_col.update_many(filter={}, update={"$set": {"booked_users": [constants.USERS[0]]}})
+    for user in constants.USERS:
+        user_col.insert_one({"user_id": user})
 
-for user in constants.USERS:
-    user_col.insert_one({"user_id": user})
+    cursor = cal_col.find({})
 
-cursor = cal_col.find({})
+    for doc in cursor:
+        print(doc)
 
-for doc in cursor:
-    print(doc)
+    cursor = user_col.find({})
 
-cursor = user_col.find({})
-
-for doc in cursor:
-    print(doc)
+    for doc in cursor:
+        print(doc)
