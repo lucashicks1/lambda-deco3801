7b1144e8421941f5d716efba0587521332463909
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-19 00:00:03 +1000

    Adds example request and response bodies.

---
+++ build/db-handler/app/examples/whiteboard_payloads.py
@@ -0,0 +1,14 @@
+POST_WHITEBOARD = {
+    "body": [
+        {
+            "day": "Monday",
+            "time_slot": 0,
+            "data": "captured text"
+        },
+        {
+            "day": "Tuesday",
+            "time_slot": 0,
+            "data": "text"
+        }
+    ]
+}
