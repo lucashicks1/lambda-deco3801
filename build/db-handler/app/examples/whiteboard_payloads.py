6be0dd4f8dcc0ead60ad86405b8177106681e527
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-19 00:34:57 +1000

    Adds example for whiteboard router.

--- build/db-handler/app/examples/whiteboard_payloads.py
+++ build/db-handler/app/examples/whiteboard_payloads.py
@@ -12,3 +12,26 @@ POST_WHITEBOARD = {
         }
     ]
 }
+
+RESPONSE_WHITEBOARD = {
+    "body": [
+        {
+            "day": "monday",
+            "time": "00:00",
+            "slot_num": 0,
+            "booked_users": [
+                "user_2",
+                "user_3",
+                "user_1"
+            ]
+        },
+        {
+            "day": "tuesday",
+            "time": "00:00",
+            "slot_num": 0,
+            "booked_users": [
+                "user_1"
+            ]
+        }
+    ]
+}
