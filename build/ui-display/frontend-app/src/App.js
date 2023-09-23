e03bd0b1af5faf2e857fdcc788e00a81288f31be
alilaherty <ali-dotcom@outlook.com>
2023-09-23 15:22:13 +1000

    adding date widget & testing my merge skills <3

--- build/ui-display/frontend-app/src/App.js
+++ build/ui-display/frontend-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DateWidget from './components/widgets/DateWidget';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {"hello, world!"}
+        <DateWidget />
       </header>
     </div>
   );
