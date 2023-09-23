219783b2ffd6484594432b8da38a308adce016e2
mayabaxx <mayabaxx@gmail.com>
2023-09-23 16:09:57 +1000

    dummy background boxes
    #sky #grass #css #gradient

--- build/ui-display/frontend-app/src/App.js
+++ build/ui-display/frontend-app/src/App.js
@@ -11,6 +11,12 @@ function App() {
         <DateWidget />
       </header>
       <ClockWidget />
+      <div className="Sky-box">
+        here's the sky
+        <div className='Grass-box'>
+          here's the grass :D
+        </div>
+      </div>
     </div>
   );
 }
