ab5f580680ca96038a30b7592d180bf047e12011
alilaherty <ali-dotcom@outlook.com>
2023-10-02 11:59:04 +1000

    fix issue with loading flag and add middleware

--- build/db-handler/app/main.py
+++ build/db-handler/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, responses
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import figures_router, whiteboard_router, display_router
 from app.dependencies.database import cal_col
 from app import help_scripts
-
 tags_metadata = [
     {
         "name": "Display",
@@ -18,27 +18,36 @@ tags_metadata = [
     }
 ]
 
+origins = [
+    "http://0.0.0.0:3000",
+    "http://localhost:3000"
+]
+
 app = FastAPI(
     title="Lambda DB Handler",
     openapi_tags=tags_metadata
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(figures_router.router)
 app.include_router(whiteboard_router.router)
 app.include_router(display_router.router)
-
-
 @app.get("/", summary="Default landing page which will redirect you to the docs")
 async def main():
     # Redirects you to doc page
     return responses.RedirectResponse("/docs")
-
-
 @app.get("/reset", summary="Resets the state of the database")
 async def reset(reset: bool = False):
     if reset:
         help_scripts.reset_db()
-
 @app.get("/dump", summary="DUMPS THE MONGODB FOR TESTING")
 async def dump():
     return {"body": list(cal_col.find({}, {"_id": 0}))}
--- build/ui-display/frontend-app/src/components/widgets/BusiestWidget.js
+++ build/ui-display/frontend-app/src/components/widgets/BusiestWidget.js
@@ -14,10 +14,11 @@ export default function BusiestWidget() {
             if (data) {
                 const times = Object.values(data);
                 setBusiest(Object.entries(data).filter(([key, value]) => value == Math.max(...times)).map(([key, value]) => key));
-                if (busiest.length > 0) {
-                    setLoading(false);
-                }
-            }   
+                
+            } 
+            if (busiest.length > 0) {
+                setLoading(false);
+            }
         };
 
         fetchData();
