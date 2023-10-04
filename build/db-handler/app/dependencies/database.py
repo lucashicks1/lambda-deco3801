1acdde1b2d3bd14efce8eb22dea058ec45881692
Lucas Hicks <lucasjhicks1@gmail.com>
2023-10-04 16:05:19 +1000

    OMG CLOUDgit status! THIS IS AMAZING

--- build/db-handler/app/dependencies/database.py
+++ build/db-handler/app/dependencies/database.py
@@ -1,10 +1,25 @@
-from pymongo import MongoClient, collection
+from pymongo import MongoClient, collection, server_api
+from os import environ
+
+DB_USER = environ.get("DB_USER")
+DB_PASS = environ.get("DB_PASS")
 
 DB_HOST = "localhost"
 DB_PORT = 27017
 DB_NAME = "mydb"
 
-client = MongoClient(DB_HOST, DB_PORT)
+args = "retryWrites=true&w=majority&?tls=true&tlsCAFile=app/cert.pem"
+uri = f"mongodb+srv://lambdadb:{DB_PASS}@lambdacluster.vch8k4d.mongodb.net/?{args}"
+
+client: MongoClient = MongoClient(uri, server_api=server_api.ServerApi("1"))
+
+try:
+    client.admin.command("ping")
+    print("Pinged db, and successful")
+except Exception as e:
+    print(e)
+    print("Not successful ping, swapping to local version")
+    client = MongoClient(DB_HOST, DB_PORT)
 
 cal_col: collection = client[DB_NAME]["calendar"]
 user_col: collection = client[DB_NAME]["users"]
