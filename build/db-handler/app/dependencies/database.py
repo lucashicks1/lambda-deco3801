f68a2a3452b1691436f714bbc8f71e912b4f508f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-10-12 00:00:26 +1000

    Updates database file.

--- build/db-handler/app/dependencies/database.py
+++ build/db-handler/app/dependencies/database.py
@@ -1,14 +1,18 @@
 from pymongo import MongoClient, collection, server_api
 from os import environ
 
+# Grab db details from environment
 DB_USER = environ.get("DB_USER")
 DB_PASS = environ.get("DB_PASS")
 
+# Information needed for connecting to local db
 DB_HOST = "localhost"
 DB_PORT = 27017
 DB_NAME = "mydb"
 
 args = "retryWrites=true&w=majority&tls=true&tlsCAFile=app/cert.pem"
+
+# URI for mongodb cluster hosted on mongodb atlas
 uri = f"mongodb+srv://lambdadb:{DB_PASS}@lambdacluster.vch8k4d.mongodb.net/?{args}"
 
 client: MongoClient = MongoClient(uri, server_api=server_api.ServerApi("1"))
