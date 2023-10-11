1652f479349bec1cefb7da28ea43e2f75c0172c8
Lucas Hicks <lucasjhicks1@gmail.com>
2023-10-12 00:00:46 +1000

    Updates payload models for whiteboard endpoints.

--- build/db-handler/app/models/whiteboard_models.py
+++ build/db-handler/app/models/whiteboard_models.py
@@ -1,13 +1,16 @@
-import typing
-
 from pydantic import BaseModel
 from typing import Literal, Optional, List
 from app import constants
 
 
+# Pydantic model used to validate each timeslot passed into the whiteboard endpoints
 class TimeSlot(BaseModel):
     day: Literal[tuple(constants.DAYS)]
     time_slot: int
     data: Optional[str] = None
+    color: Optional[str] = None
+
+
+# Request payload passed into whiteboard routers
 class WhiteboardRequest(BaseModel):
     body: List[TimeSlot]
