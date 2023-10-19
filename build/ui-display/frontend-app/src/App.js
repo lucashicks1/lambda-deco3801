437d31b2ceb0144cc179364f37db2d07b860f5f4
mayabaxx <mayabaxx@gmail.com>
2023-10-19 11:46:37 +1000

    re-jig app.js (nested divs :heart_eyes: )

--- build/ui-display/frontend-app/src/App.js
+++ build/ui-display/frontend-app/src/App.js
@@ -8,23 +8,8 @@ import BusiestWidget from './components/widgets/BusiestWidget';
 import SuggestedTimeWidget from './components/widgets/SuggestedTimeWidget';
 
 function App() {
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <div className="Clock-box">
-    //     <ClockWidget />
-    //     </div>
-    //     <DateWidget />
-    //   </header>
-    //   <div className="Sky-box">
-    //     <BusiestWidget />
-    //     <SuggestedTimeWidget />
-    //     here's the sky
-    //   </div>
-    //   <div className='Grass-box'>
-    //       here's the grass :D
-    //   </div>
-    // </div>
     <div className="App">
       <div className="Sky-box">
         <div>
@@ -44,11 +29,16 @@ function App() {
           </div>
         </div>
         <div className="SuggestedTime-box">
-          {/* <SuggestedTimeWidget /> */}
+          <SuggestedTimeWidget />
+        </div>
+        <div className="shared-div">
+          <div className="left-widget">
+            <BusiestWidget />
+          </div>
         </div>
       </div>
       <div className="Grass-box">
-        {/* grass here */}
+      
       </div>
     </div>
   );
